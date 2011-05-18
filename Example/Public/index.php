f05a9299024c27d209b7092f128cd78c1d1f3739
Philippe Gerber <philippe@bigwhoop.ch>
2011-05-18 23:17:35 +0200

    Removed log files

--- Example/Public/index.php
+++ Example/Public/index.php
@@ -2,10 +2,11 @@
 namespace HynageExampleApp;
 use Hynage\Application as Application;
 
+// Define path to Hynage's library
 define('HYNAGE_LIBRARY_PATH', realpath(__DIR__ . '/../../Source'));
 
 // Include Hynage by source
-require_once realpath(__DIR__ . '/../../source/Hynage/Application.php');
+require realpath(__DIR__ . '/../../Source/Hynage/Application.php');
 
 // Assemble path to config file
 $config = realpath(__DIR__ . '/../Application/Config/Default.php');
